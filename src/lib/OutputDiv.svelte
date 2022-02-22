723c4e1b91cd2a81f6ef6906d888b6871ced0c7e
J3Jab <jason_jan_jabanes@dlsu.edu.ph>
2022-02-23 06:18:17 +0800

    Updates

--- src/lib/OutputDiv.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-  export let title: string;
-  export let value: string;
-</script>
-
-<div class="my-10">
-  <p class="uppercase text-center text-2xl font-extrabold mb-2">{title}</p>
-  <p class="text-center text-xl font-bold">{value}</p>
-</div>
--- src/lib/RoundOffMethods.d.ts
+++
@@ -1,6 +0,0 @@
-export enum RoundOffMethods {
-  Truncate = "Truncate",
-  RoundToNearest = "Round To Nearest",
-  RoundUp = "Round Up",
-  RoundDown = "Round Down",
-}
